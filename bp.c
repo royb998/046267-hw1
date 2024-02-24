70e62ac900aecd16d2b57c07b16a2d3721ef8250
Roy Bar-On <royb997@gmail.com>
2024-02-24 23:19:35 +0200

    Reformats and fixes

--- bp.c
+++ bp.c
@@ -6,22 +6,31 @@
 #include "string.h"
 #include "math.h"
 
+/* ----- Macros ----- */
+
+#define FREE(_ptr)          \
+do {                        \
+    if ((_ptr) != NULL) {   \
+        free(_ptr);         \
+}} while(0)
+
 #define INSTRUCTION_SIZE  32
 
 /* ----- Structs ----- */
 
-struct btb_entry
+typedef struct btb_entry
 {
-    int valid;
-    int tag;
-    int target;
-} btb_entry;
+    bool valid;
+    size_t tag;
+    size_t target;
+} btb_entry_t;
 
 struct branch_target_buffer
 {
     struct btb_entry * btbTable;
     unsigned * history;
-    unsigned ** tableState;
+    size_t * tableState;
+//    unsigned ** tableState;
 
     unsigned int btbSize;       // num of lines in btb table
     unsigned int historySize;   // num history bit
@@ -33,12 +42,11 @@ struct branch_target_buffer
     int Shared;                 // 0, 1 or 2
 
     SIM_stats stats;
-
 };
 
 /* ----- Globals ----- */
 
-struct branch_target_buffer * btb;   // global btb
+struct branch_target_buffer g_btb;
 
 /* ----- Internal methods ----- */
 
@@ -52,7 +60,7 @@ unsigned getTagFromPc(unsigned btbSize, unsigned tagSize, uint32_t pc)
 unsigned getBtbIndex(uint32_t pc)
 {
     unsigned index = pc >> 2;
-    return index % (unsigned)(pow(2, log2(btb->btbSize)));
+    return index % (unsigned)(pow(2, log2(g_btb.btbSize)));
 }
 
 unsigned getStateIndex(uint32_t pc)
@@ -60,30 +68,130 @@ unsigned getStateIndex(uint32_t pc)
     unsigned shareValue;
     unsigned n;
 
-    if (btb->Shared == 1)
+    if (g_btb.Shared == 1)
     {
-        shareValue = (pc >> 2) % (unsigned)(pow(2, btb->historySize));
+        shareValue = (pc >> 2) % (unsigned)(pow(2, g_btb.historySize));
     }
-    else if (btb->Shared == 2)
+    else if (g_btb.Shared == 2)
     {
-        shareValue = (pc >> 16) % (unsigned)(pow(2, btb->historySize));
+        shareValue = (pc >> 16) % (unsigned)(pow(2, g_btb.historySize));
     }
     else
     {
         shareValue = 0;
     }
 
-    if (btb->isGlobalHist)
+    if (g_btb.isGlobalHist)
     {
-        n = *btb->history;
+        n = *g_btb.history;
     }
     else
     {
         unsigned btbIndex = getBtbIndex(pc);
-        n = btb->history[btbIndex];
+        n = g_btb.history[btbIndex];
     }
 
     return n ^ shareValue;
+}
+
+size_t get_state_table_size(size_t history_size)
+{
+    return 1 << history_size;
+}
+
+size_t get_branch_state(uint32_t pc)
+{
+    size_t table_index = g_btb.isGlobalTableState ? 0 : getBtbIndex(pc);
+    size_t table_base = table_index * get_state_table_size(g_btb.historySize);
+    size_t state_index = getStateIndex(pc);
+
+    // Only return lowest 2 bytes.
+    return g_btb.tableState[table_base + state_index] & 0x3;
+}
+
+/**
+ * @brief Initialize the history register(s) for the predictor.
+ * @param btbSize (IN)      Number of BTB entries.
+ * @param historySize (IN)  Size of each history register.
+ * @param isGlobalHist (IN) True if global history register should be used
+ *                      instead of individual ones; False otherwise.
+ * @param total_size (INOUT) Total (theoretical) memory size used by predictor.
+ * @return 0 for success, -1 for failed allocation.
+ */
+int
+initialize_history(
+    unsigned int btbSize,
+    unsigned int historySize,
+    bool isGlobalHist,
+    size_t * total_size)
+{
+    // If global history, keep only 1 history register.
+    size_t history_register_count = isGlobalHist ? 1 : btbSize;
+
+    g_btb.history = calloc(history_register_count, sizeof(size_t));
+
+    if (g_btb.history == NULL)
+    {
+        return -1;
+    }
+
+    // Update total size.
+    (*total_size) += history_register_count * historySize;
+
+    return 0;
+}
+
+/**
+ * @brief Allocate the prediction state machine(s) for the predictor.
+ * @param btbSize (IN)      Number of BTB entries.
+ * @param historySize (IN)  Size of each history register.
+ * @param isGlobalTable (IN) True if global prediction state machine should be
+ *                      used instead of individual ones; False otherwise.
+ * @param total_size (INOUT) Total (theoretical) memory size used by predictor.
+ * @return 0 for success, -1 for failed allocation.
+ */
+int
+allocate_states(
+    unsigned int btbSize,
+    unsigned int historySize,
+    bool isGlobalTable,
+    size_t * total_size)
+{
+    size_t state_table_size = 1 << historySize;
+
+    // Only one entry if global table.
+    size_t entry_count = isGlobalTable ? 1 : btbSize;
+
+    // Allocate the entries - for each entry, allocate table of state machines.
+    g_btb.tableState = malloc(entry_count * state_table_size * sizeof(size_t));
+    if (g_btb.tableState == NULL)
+    {
+        return -1;
+    }
+
+    // 2 bits for every SM,
+    (*total_size) += entry_count * state_table_size * 2;
+
+    return 0;
+}
+
+void
+initialize_states(
+    unsigned int btbSize,
+    unsigned int historySize,
+    unsigned int fsmState,
+    bool isGlobalTable)
+{
+    size_t entry_count = isGlobalTable ? 1 : btbSize;
+    size_t state_table_size = get_state_table_size(historySize);
+
+    for (int i = 0; i < entry_count; ++i)
+    {
+        for (int j = 0; j < state_table_size; j++)
+        {
+            g_btb.tableState[i * state_table_size + j] = fsmState;
+        }
+    }
 }
 
 /* ----- External methods ----- */
@@ -97,107 +205,62 @@ int BP_init(
     bool isGlobalTable,
     int Shared)
 {
-    unsigned int size = 0;
+    int return_value;
 
-    // creating btbTable and zeroing
-    btb->btbTable = malloc(btbSize * (1 + tagSize + INSTRUCTION_SIZE));
-    if (btb->btbTable->tag == NULL)
+    // Theoretical total_size for each BTB entry.
+    size_t entry_size = 1 + tagSize + INSTRUCTION_SIZE;
+
+    // Total size that should be used by the predictor.
+    size_t total_size = 0;
+
+    // Allocate the entries.
+    g_btb.btbTable = calloc(btbSize, sizeof(btb_entry_t));
+    if (NULL == g_btb.btbTable)
     {
-        free(btb);
         return -1;
     }
-    memset(btb->btbTable, 0, sizeof(*btb->btbTable));
-    size += 1 + tagSize + INSTRUCTION_SIZE;
 
-    // creating History and zeroing
-    if (isGlobalHist)
+    total_size += btbSize * entry_size;
+
+    // Allocate history registers.
+    return_value = initialize_history(btbSize,
+                                      historySize,
+                                      isGlobalHist,
+                                      &total_size);
+    if (0 != return_value)
     {
-        // if global history, size = 8
-        btb->history = malloc(sizeof(unsigned));
-
-        if (btb->history == NULL)
-        {
-            free(btb->btbTable);
-            free(btb);
-            return -1;
-        }
-        size += historySize;
+        FREE(g_btb.btbTable);
+        return -1;
     }
-    else
-    {
-        // if local History, size = 8 * btb_size
-        btb->history = malloc(sizeof(unsigned) * btbSize);
 
-        if (btb->history == NULL)
-        {
-            free(btb->btbTable);
-            free(btb);
-            return -1;
-        }
-        size += btbSize * historySize;
-    }
-    memset(btb->history, 0, sizeof(*btb->history));
-
-    // creating state table
-    if (isGlobalTable)
+    // Allocate & fill prediction state machines.
+    return_value = allocate_states(btbSize,
+                                   historySize,
+                                   isGlobalTable,
+                                   &total_size);
+    if (0 != return_value)
     {
-        // if stateTable is global, size = 2^history_size * 2
-        btb->tableState = malloc(sizeof(unsigned) * (size_t)pow(2, historySize));
-        if (btb->tableState == NULL)
-        {
-            free(btb->history);
-            free(btb->btbTable);
-            free(btb);
-            return -1;
-        }
-        size += (unsigned)pow(2, (historySize + 1));
-    }
-    else
-    {
-        // if stateTable is local, size = btb_size * 2^history_size * 2
-        btb->tableState = malloc(sizeof(unsigned) * btbSize * (size_t)pow(2, historySize));
-        if (btb->tableState == NULL)
-        {
-            free(btb->history);
-            free(btb->btbTable);
-            free(btb);
-            return -1;
-        }
-        size += btbSize * (unsigned)pow(2, (historySize + 1));
+        FREE(g_btb.btbTable);
+        FREE(g_btb.history);
+        return -1;
     }
 
     // filling table state with begin state
-    if (isGlobalTable)
-    {
-        for (int i = 0; i < pow(2, historySize); i++)
-        {
-            btb->tableState[0][i] = fsmState;
-        }
-    }
-    else
-    {
-        for (int j = 0; j < btbSize; j++)
-        {
-            for (int i = 0; i < pow(2, historySize); i++)
-            {
-                btb->tableState[j][i] = fsmState;
-            }
-        }
-    }
+    initialize_states(btbSize, historySize, fsmState, isGlobalTable);
 
     // initialising values
-    btb->btbSize = btbSize;
-    btb->historySize = historySize;
-    btb->tagSize = tagSize;
-    btb->beginState = fsmState;
-    btb->isGlobalHist = isGlobalHist;
-    btb->isGlobalTableState = isGlobalTable;
-    btb->Shared = Shared;
+    g_btb.btbSize = btbSize;
+    g_btb.historySize = historySize;
+    g_btb.tagSize = tagSize;
+    g_btb.beginState = fsmState;
+    g_btb.isGlobalHist = isGlobalHist;
+    g_btb.isGlobalTableState = isGlobalTable;
+    g_btb.Shared = Shared;
 
     // initializing stats
-    btb->stats.br_num = 0;
-    btb->stats.flush_num = 0;
-    btb->stats.size = size;
+    g_btb.stats.br_num = 0;
+    g_btb.stats.flush_num = 0;
+    g_btb.stats.size = total_size;
 
     return 0;
 }
@@ -205,28 +268,23 @@ int BP_init(
 bool BP_predict(uint32_t pc, uint32_t * dst)
 {
     unsigned btbIndex = getBtbIndex(pc);
-    unsigned tableStateIndex = getStateIndex(pc);
-    unsigned currentState = 0;
+    unsigned currentState;
 
-    if (btb->btbTable[btbIndex].valid == 0 ||
-        btb->btbTable[btbIndex].tag != getTagFromPc(btb->btbSize, btb->tagSize, pc))
+    // If the BTB isn't valid, or if the tags don't match, there's no prediction
+    // for the given pc, assume not taken.
+    if (!g_btb.btbTable[btbIndex].valid ||
+        g_btb.btbTable[btbIndex].tag != getTagFromPc(g_btb.btbSize,
+                                                     g_btb.tagSize, pc))
     {
         *dst = pc + 4;
         return false;
     }
 
-    if (btb->isGlobalTableState)
-    {
-        currentState = *(btb->tableState[tableStateIndex]);
-    }
-    else
-    {
-        currentState = *(btb->tableState[btbIndex * (unsigned)pow(2, btb->historySize) + tableStateIndex]);
-    }
+    currentState = get_branch_state(pc);
 
     if (currentState > 1)
     {
-        *dst = btb->btbTable[btbIndex].target;
+        *dst = g_btb.btbTable[btbIndex].target;
         return true;
     }
     else
@@ -238,17 +296,16 @@ bool BP_predict(uint32_t pc, uint32_t * dst)
 
 void BP_update(uint32_t pc, uint32_t targetPc, bool taken, uint32_t pred_dst)
 {
-    return;
 }
 
 void BP_GetStats(SIM_stats * curStats)
 {
-    curStats->br_num = btb->stats.br_num;
-    curStats->flush_num = btb->stats.flush_num;
-    curStats->size = btb->stats.size;
+    curStats->br_num = g_btb.stats.br_num;
+    curStats->flush_num = g_btb.stats.flush_num;
+    curStats->size = g_btb.stats.size;
 
-    free(btb->btbTable);
-    free(btb->history);
-    free(btb->tableState);
+    FREE(g_btb.btbTable);
+    FREE(g_btb.history);
+    FREE(g_btb.tableState);
 }
 
